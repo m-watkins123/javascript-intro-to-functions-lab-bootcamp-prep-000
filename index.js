e3a6e6bdedd5d0fde9c5fbd2ec3e01c3c1b84090
manassah Watkins <manassahwatkins@gmail.com>
2017-12-30 15:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout(string){
   return string.toUpperCase()
 }
+function whisper(string){
+  return string.tolowercase()
+}
+function logshout(string){
+  console.log("all caps")
+}
+function logwhisper(string){
+  console.log("all lowercase")
+}
+function sayHiToGrandma(string){
+  return ("I cant hear you!")
+}
